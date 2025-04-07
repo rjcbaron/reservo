433cc92c81d1f78a14ea1d6192f01120c92d6ab8
rjcbaron <baronryanjoshcedrick@gmail.com>
2025-04-08 07:33:48 +0800

    feat: added login and register pages

--- src/router/index.js
+++ src/router/index.js
@@ -1,21 +1,19 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import LoginView from '@/views/auth/auth/LoginView.vue'
+import RegisterView from '@/views/auth/auth/RegisterView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: HomeView,
+      name: 'login',
+      component: LoginView,
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue'),
+      path: '/register',
+      name: 'register',
+      component: RegisterView,
     },
   ],
 })
---
+++ src/views/auth/auth/LoginView.vue
@@ -0,0 +1,69 @@
+<script setup>
+import { ref } from 'vue'
+import { RouterLink } from 'vue-router'
+
+const theme = ref('light')
+
+function onClick() {
+  theme.value = theme.value === 'light' ? 'dark' : 'light'
+}
+</script>
+
+<template>
+  <v-responsive class="border rounded">
+    <v-app :theme="theme">
+      <v-app-bar class="px-3">
+        <v-spacer></v-spacer>
+
+        <v-btn
+          :prepend-icon="theme === 'light' ? 'mdi-weather-sunny' : 'mdi-weather-night'"
+          text="Toggle Theme"
+          slim
+          @click="onClick"
+        ></v-btn>
+      </v-app-bar>
+
+      <v-main>
+        <v-container>
+          <v-row>
+            <v-col cols="12" md="6">
+              <v-card
+                class="mx-auto"
+                prepend-icon="mdi mdi-account-badge"
+                subtitle="Log in to your account"
+              >
+                <template v-slot:title>
+                  <span class="font-weight-black">Welcome to Reservo</span>
+                </template>
+
+                <v-card-text class="bg-surface-light pt-4">
+                  <v-form fast-fail @submit.prevent>
+                    <v-text-field label="Email" variant="outlined"></v-text-field>
+
+                    <v-text-field
+                      label="Password"
+                      type="password"
+                      variant="outlined"
+                    ></v-text-field>
+
+                    <v-btn class="mt-2" type="submit" block>Log In</v-btn>
+                  </v-form>
+
+                  <v-divider class="my-5"></v-divider>
+                  <h5 class="text-center">
+                    Don't have account?
+                    <RouterLink class="text-primary" to="/register">
+                      Click here to register</RouterLink
+                    >
+                  </h5>
+                </v-card-text>
+              </v-card>
+            </v-col>
+          </v-row>
+        </v-container>
+      </v-main>
+
+      <v-footer border app> 2025 - Reservo</v-footer>
+    </v-app>
+  </v-responsive>
+</template>
