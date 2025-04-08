ef5beb2767ca9046412f268f021372b2e84f3410
rjcbaron <baronryanjoshcedrick@gmail.com>
2025-04-08 10:13:17 +0800

    feat:am stupid this is the actual persistent theme sorry guys

--- src/components/layout/AppLayout.vue
+++ src/components/layout/AppLayout.vue
@@ -5,7 +5,7 @@ const theme = ref(localStorage.getItem('theme') ?? 'light')
 
 function onClick() {
   theme.value = theme.value === 'light' ? 'dark' : 'light'
-  localStorage.setItem('theme', theme.valuegit)
+  localStorage.setItem('theme', theme.value)
 }
 </script>
 
