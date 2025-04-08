3fc1b6fb740a82e48418575580f2962101301c45
rjcbaron <baronryanjoshcedrick@gmail.com>
2025-04-08 10:07:47 +0800

    feat:persistent theme added

--- src/components/layout/AppLayout.vue
+++ src/components/layout/AppLayout.vue
@@ -1,7 +1,7 @@
 <script setup>
 import { ref } from 'vue'
 
-const theme = ref('light')
+const theme = ref(localStorage.getItem('theme') ?? 'light')
 
 function onClick() {
   theme.value = theme.value === 'light' ? 'dark' : 'light'
