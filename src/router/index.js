c13dd18e0e68f6514a29ce06df12befcb366ea50
diannealaan <dianne.alaan@carsu.edu.ph>
2025-04-18 23:47:23 +0800

    temporary profileview page

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import LoginView from '@/views/auth/auth/LoginView.vue'
 import RegisterView from '@/views/auth/auth/RegisterView.vue'
+import ProfileView from '@/views/auth/auth/ProfileView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -14,6 +15,11 @@ const router = createRouter({
       path: '/register',
       name: 'register',
       component: RegisterView,
+    },
+    {
+      path: '/profile',
+      name: 'profile',
+      component: ProfileView,
     },
   ],
 })
