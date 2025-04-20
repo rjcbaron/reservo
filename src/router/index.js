53b7b138bfb42225e270ad824196b9b245faa162
aubrey <padasaubrey@gmail.com>
2025-04-20 14:48:32 +0800

    Added partial home page

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 import LoginView from '@/views/auth/auth/LoginView.vue'
 import RegisterView from '@/views/auth/auth/RegisterView.vue'
 import ProfileView from '@/views/auth/auth/ProfileView.vue'
+import HomeView from '@/views/HomeView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -20,6 +21,11 @@ const router = createRouter({
       path: '/profile',
       name: 'profile',
       component: ProfileView,
+    },
+    {
+      path: '/home',
+      name: 'home',
+      component: HomeView,
     },
   ],
 })
