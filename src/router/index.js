79b5b508927fdd7b944aa8a48c6ad07dcfa13f25
rjcbaron <baronryanjoshcedrick@gmail.com>
2025-04-20 19:29:47 +0800

    fix: homeview.vue error

--- src/router/index.js
+++ src/router/index.js
@@ -2,7 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 import LoginView from '@/views/auth/auth/LoginView.vue'
 import RegisterView from '@/views/auth/auth/RegisterView.vue'
 import ProfileView from '@/views/auth/auth/ProfileView.vue'
-import HomeView from '@/views/HomeView.vue'
+import HomeView from '@/views/auth/auth/HomeView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
