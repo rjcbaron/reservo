b08aaa376821e99eca76cc2f4659dd0e0cecaba0
rjcbaron <baronryanjoshcedrick@gmail.com>
2025-04-21 07:10:40 +0800

    fix: All buttons should navigate to their respective pages

--- src/views/auth/auth/ProfileView.vue
+++ src/views/auth/auth/ProfileView.vue
@@ -22,40 +22,43 @@ function cancelEdit() {
   // Reset to original values if needed
 }
 </script>
+
 <template>
   <AppLayout>
-<template>
-  <v-container class="d-flex justify-center align-center" style="min-height: 100vh">
-    <v-card class="pa-6" max-width="600" width="100%">
-      <v-card-title class="text-h5 text-center">User Profile</v-card-title>
-      <v-divider class="my-4" />
+    <template #content>
+      <v-container class="d-flex justify-center align-center" style="min-height: 100vh">
+        <v-card class="pa-6" max-width="600" width="100%">
+          <v-card-title class="text-h5 text-center">User Profile</v-card-title>
+          <v-divider class="my-4" />
 
-      <v-card-text class="text-center">
-        <v-avatar size="100" class="mx-auto mb-4">
-          <v-img :src="user.avatar" alt="Avatar" />
-        </v-avatar>
+          <v-card-text class="text-center">
+            <v-avatar size="100" class="mx-auto mb-4">
+              <v-img :src="user.avatar" alt="Avatar" />
+            </v-avatar>
 
-        <v-form v-if="isEditing" class="text-left">
-          <v-text-field v-model="user.firstName" label="First Name" outlined dense />
-          <v-text-field v-model="user.lastName" label="Last Name" outlined dense />
-          <v-text-field v-model="user.email" label="Email" outlined dense />
-        </v-form>
+            <v-form v-if="isEditing" class="text-left">
+              <v-text-field v-model="user.firstName" label="First Name" outlined dense />
+              <v-text-field v-model="user.lastName" label="Last Name" outlined dense />
+              <v-text-field v-model="user.email" label="Email" outlined dense />
+            </v-form>
 
-        <div v-else>
-          <h3>{{ user.firstName }} {{ user.lastName }}</h3>
-          <p>{{ user.email }}</p>
-        </div>
-      </v-card-text>
+            <div v-else>
+              <h3>{{ user.firstName }} {{ user.lastName }}</h3>
+              <p>{{ user.email }}</p>
+            </div>
+          </v-card-text>
 
-      <v-card-actions class="justify-center">
-        <v-btn v-if="!isEditing" color="primary" @click="isEditing = true"> Edit Profile </v-btn>
-        <div v-else>
-          <v-btn color="success" @click="saveChanges">Save</v-btn>
-          <v-btn color="error" variant="outlined" @click="cancelEdit">Cancel</v-btn>
-        </div>
-      </v-card-actions>
-    </v-card>
-  </v-container>
-</template>
-</AppLayout>
+          <v-card-actions class="justify-center">
+            <v-btn v-if="!isEditing" color="primary" @click="isEditing = true">
+              Edit Profile
+            </v-btn>
+            <div v-else>
+              <v-btn color="success" @click="saveChanges">Save</v-btn>
+              <v-btn color="error" variant="outlined" @click="cancelEdit">Cancel</v-btn>
+            </div>
+          </v-card-actions>
+        </v-card>
+      </v-container>
+    </template>
+  </AppLayout>
 </template>
