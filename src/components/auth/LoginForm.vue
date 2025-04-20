f38bc35464845024a6e01ede1b6963d38630d2d9
rjcbaron <baronryanjoshcedrick@gmail.com>
2025-04-20 19:22:04 +0800

    feat:  added icons on the text boxes for login and register and password privacy

--- src/components/auth/LoginForm.vue
+++ src/components/auth/LoginForm.vue
@@ -1,11 +1,22 @@
+<script setup>
+  import { ref } from 'vue'
+
+  const isPasswordvisible = ref(false)
+</script>
+
 <template>
   <v-form fast-fail @submit.prevent>
-                <v-text-field label="Email" variant="outlined"></v-text-field>
+    <v-text-field label="Email" variant="outlined" prepend-inner-icon="mdi-email"></v-text-field>
 
-                <v-text-field label="Password" type="password" variant="outlined"></v-text-field>
+    <v-text-field
+      label="Password"
+      :type="isPasswordvisible ? 'text' : 'password'"
+      :append-inner-icon="isPasswordvisible ? 'mdi-eye-off' : 'mdi-eye-outline'"
+      variant="outlined"
+      prepend-inner-icon="mdi-lock"
+      @click:append-inner="isPasswordvisible = !isPasswordvisible"
+    ></v-text-field>
 
-                <v-btn class="mt-2" type="submit" block color="primary" prepend-icon="mdi-login"
-                  >Log In</v-btn
-                >
-              </v-form>
+    <v-btn class="mt-2" type="submit" block color="primary" prepend-icon="mdi-login">Log In</v-btn>
+  </v-form>
 </template>
--- src/components/auth/RegisterForm.vue
+++ src/components/auth/RegisterForm.vue
@@ -1,29 +1,48 @@
+<script setup>
+  import { ref } from 'vue'
+
+  const isPasswordvisible = ref(false)
+</script>
+
 <template>
-   <v-form fast-fail @submit.prevent>
-                <v-text-field label="First Name" variant="outlined"></v-text-field>
-                <v-text-field label="Last Name" variant="outlined"></v-text-field>
-                <v-text-field label="Email" variant="outlined"></v-text-field>
+  <v-form fast-fail @submit.prevent>
+    <v-text-field
+      label="First Name"
+      variant="outlined"
+      prepend-inner-icon="mdi-account-file-text"
+    ></v-text-field>
+    <v-text-field
+      label="Last Name"
+      variant="outlined"
+      prepend-inner-icon="mdi-account-file-text"
+    ></v-text-field>
+    <v-text-field label="Email" variant="outlined" prepend-inner-icon="mdi-email"></v-text-field>
 
-                <v-text-field label="Password" type="password" variant="outlined"></v-text-field>
+    <v-text-field
+      label="Password"
+      :type="isPasswordvisible ? 'text' : 'password'"
+      :append-inner-icon="isPasswordvisible ? 'mdi-eye-off' : 'mdi-eye-outline'"
+      variant="outlined"
+      prepend-inner-icon="mdi-lock"
+      @click:append-inner="isPasswordvisible = !isPasswordvisible"
+    ></v-text-field>
 
-                <v-text-field
-                  label="Password Confirmation"
-                  type="password"
-                  variant="outlined"
-                ></v-text-field>
+    <v-text-field
+      label="Password Confirmation"
+      t:type="isPasswordvisible ? 'text' : 'password'"
+      :append-inner-icon="isPasswordvisible ? 'mdi-eye-off' : 'mdi-eye-outline'"
+      variant="outlined"
+      prepend-inner-icon="mdi-lock"
+      @click:append-inner="isPasswordvisible = !isPasswordvisible"
+    ></v-text-field>
 
-                <v-divider class="my-5"></v-divider>
-                <h5 class="text-center">
-                  Already have acount?
-                  <RouterLink class="text-primary" to="/"> Click here to log in</RouterLink>
-                </h5>
-                <v-btn
-                  class="mt-2"
-                  type="submit"
-                  block
-                  color="primary"
-                  prepend-icon="mdi-account-plus"
-                  >Sign Up</v-btn
-                >
-              </v-form>
+    <v-divider class="my-5"></v-divider>
+    <h5 class="text-center">
+      Already have acount?
+      <RouterLink class="text-primary" to="/"> Click here to log in</RouterLink>
+    </h5>
+    <v-btn class="mt-2" type="submit" block color="primary" prepend-icon="mdi-account-plus"
+      >Sign Up</v-btn
+    >
+  </v-form>
 </template>
