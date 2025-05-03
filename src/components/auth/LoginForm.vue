441f940803e97684bdb1395cef932ec875d9d983
rjcbaron <baronryanjoshcedrick@gmail.com>
2025-05-03 14:57:21 +0800

    fix: login authentication should work now

--- src/components/auth/LoginForm.vue
+++ src/components/auth/LoginForm.vue
@@ -1,7 +1,10 @@
 <script setup>
 import { ref } from 'vue'
+import { useRouter } from 'vue-router'
+import { supabase, formActionDefault } from '@/utils/supabase.js'
 import { requiredValidator, emailValidator } from '@/utils/validators'
 
+const router = useRouter()
 const isPasswordvisible = ref(false)
 const refVForm = ref()
 
@@ -11,25 +14,42 @@ const formDataDefault = {
 }
 
 const formData = ref({
-  ...formDataDefault
+  ...formDataDefault,
 })
 
-const onSubmit = () => {
-  alert(formData.value.email)
+const formAction = ref({ ...formActionDefault })
+
+const onSubmit = async () => {
+  formAction.value = { ...formActionDefault }
+  formAction.value.formProcess = true
+
+  const { data, error } = await supabase.auth.signInWithPassword({
+    email: formData.value.email,
+    password: formData.value.password,
+  })
+
+  if (error) {
+    console.log(error)
+    formAction.value.formErrorMessage = error.message
+    formAction.value.formStatus = error.status
+  } else if (data.session) {
+    router.replace('/home')
+  }
+
+  formAction.value.formProcess = false
 }
 
 const onFormSubmit = () => {
   refVForm.value?.validate().then(({ valid }) => {
-     if(valid)
-     onSubmit()
+    if (valid) onSubmit()
   })
 }
 </script>
 
 <template>
-  <v-form ref="refVForm"  @submit.prevent="onFormSubmit">
+  <v-form ref="refVForm" @submit.prevent="onFormSubmit">
     <v-text-field
-    v-model="formData.email"
+      v-model="formData.email"
       label="Email"
       variant="outlined"
       prepend-inner-icon="mdi-email"
@@ -37,7 +57,7 @@ const onFormSubmit = () => {
     ></v-text-field>
 
     <v-text-field
-    v-model="formData.password"
+      v-model="formData.password"
       label="Password"
       :type="isPasswordvisible ? 'text' : 'password'"
       :append-inner-icon="isPasswordvisible ? 'mdi-eye-off' : 'mdi-eye-outline'"
@@ -47,6 +67,16 @@ const onFormSubmit = () => {
       :rules="[requiredValidator]"
     ></v-text-field>
 
-    <v-btn class="mt-2" type="submit" block color="primary" prepend-icon="mdi-login">Log In</v-btn>
+    <v-btn
+      class="mt-2"
+      type="submit"
+      block
+      color="primary"
+      prepend-icon="mdi-login"
+      :disabled="formAction.formProcess"
+      :loading="formAction.formProcess"
+    >
+      Log In
+    </v-btn>
   </v-form>
 </template>
